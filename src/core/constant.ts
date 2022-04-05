faa01c875224be3792f99d71f79f83a6774f7e12
Loc <hoanglocst900@gmail.com>
2022-04-05 23:32:06 +0700

    ADD - filter users

--- src/core/constant.ts
+++ src/core/constant.ts
@@ -1,31 +1,11 @@
-interface AuthControllerConstant {
-    /**
-     *@description time of cookie when user login
-     *@example  1h = 60 * 60 * 1000
-     */
-    readonly loginCookieTime: number;
-
-    /**
-     *@description time of cookie when users register
-     *@example  1h = 60 * 60 * 1000
-     */
-    readonly registerCookieTime: number;
-
-    /**
-     *@description time of cookie when users login with google
-     *@description  milliseconds 1h = 60 * 60 * 1000
-     */
-    readonly googleUserCookieTime: number;
-}
-
-interface Constant {
-    authController: AuthControllerConstant;
-}
-
-export const constant: Constant = {
+export const constant = Object.freeze({
     authController: {
         loginCookieTime: 1000 * 60 * 60 * 24 * 30,
         registerCookieTime: 1000 * 60 * 60 * 24 * 30,
         googleUserCookieTime: 1000 * 60 * 60 * 24 * 30,
     },
-};
+    userController: {
+        defaultCurrentPage: 1,
+        defaultPageSize: 4,
+    },
+});
