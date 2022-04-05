e46798c4b820ecc006275c5c0f68967c9b7d4df5
Duc Dauuu <dauleduc2@gmail.com>
2022-04-05 20:25:05 +0700

    ADD - update user information

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,16 @@
-import { Body, Controller, Get, HttpException, Param, Post, Req, Res, UseGuards, UsePipes } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, Param, Post, Put, Req, Res, UseGuards, UsePipes } from '@nestjs/common';
 
 import { UserService } from './user.service';
 import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
-import { UseGuard } from '../auth/auth.guard';
+import { AuthGuard } from '../auth/auth.guard';
 import { AuthService } from '../auth/auth.service';
 import { SendVerifyEmailDTO } from './dto/sendverifyEmail.dto';
 import { EmailService } from '../core/services';
 import { ChangePasswordDTO, vChangePasswordDTO } from './dto/changePassword.dto';
 import { JoiValidatorPipe } from 'src/core/pipe/validator.pipe';
+import { UpdateNameDTO, vUpdateNameDTO } from './dto/updateName.dto';
 
 @ApiTags('user')
 @ApiBearerAuth()
@@ -54,7 +55,7 @@ export class UserController {
     }
 
     @Get('/me')
-    @UseGuards(UseGuard)
+    @UseGuards(AuthGuard)
     async cGetMe(@Req() req: Request, @Res() res: Response) {
         return res.send(req.user);
     }
@@ -66,17 +67,31 @@ export class UserController {
         return res.send({ data: user });
     }
 
-    @Post('/change-password')
+    @Put('/password')
     @UsePipes(new JoiValidatorPipe(vChangePasswordDTO))
     async changePassword(@Body() body: ChangePasswordDTO, @Res() res: Response) {
         //get current user data
         const user = await this.userService.findUser('username', body.username);
         //check current input value is correct or not
-        if (!this.authService.decryptPassword(body.currentPassword, user.password)) {
+        const isCorrectPassword = this.authService.decryptPassword(body.currentPassword, user.password);
+        if (!isCorrectPassword) {
             throw new HttpException({ errorMessage: 'error.invalid_current_password' }, StatusCodes.BAD_REQUEST);
         }
         //change password to new password
         user.password = await this.authService.encryptPassword(body.newPassword, 10);
+        await this.userService.saveUser(user);
+        return res.send({ message: 'success' });
+    }
+
+    @Put('/name')
+    @UseGuards(AuthGuard)
+    @UsePipes(new JoiValidatorPipe(vUpdateNameDTO))
+    async updateUserInformation(@Body() body: UpdateNameDTO, @Res() res: Response, @Req() req: Request) {
+        //get current user data
+        const userId = req.user.id;
+        const user = await this.userService.findUser('id', userId);
+        // update field
+        user.name = body.value;
         await this.userService.saveUser(user);
         return res.send({ message: 'success' });
     }
