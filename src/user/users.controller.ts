9952f61048afb3ce44a41f34e7b87ee45b975cce
Loc <hoanglocst900@gmail.com>
2022-04-06 21:20:19 +0700

    ADD - users controller code test

--- src/user/users.controller.ts
+++ src/user/users.controller.ts
@@ -1,9 +1,8 @@
 import { QueryJoiValidatorPipe } from './../core/pipe/queryValidator.pipe';
 import { FilterUsersDTO, vFilterUsersDto } from './dto/filterUsers.dto';
-import { Controller, Get, HttpException, Query, Res, UsePipes } from '@nestjs/common';
+import { Controller, Get, Query, Res, UsePipes } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { Response } from 'express';
-import { StatusCodes } from 'http-status-codes';
 import { AuthService } from '../auth/auth.service';
 import { UserService } from './user.service';
 
@@ -13,20 +12,13 @@ import { UserService } from './user.service';
 export class UsersController {
     constructor(private readonly userService: UserService, private readonly authService: AuthService) {}
 
-    @Get('/')
-    async cGetAll(@Res() res: Response) {
-        const users = await this.userService.getAll();
-        if (!users) throw new HttpException({ errorMessage: 'error.not_found' }, StatusCodes.NOT_FOUND);
-        return res.send({ data: users });
-    }
-
     @Get('/search')
     @UsePipes(new QueryJoiValidatorPipe(vFilterUsersDto))
     async filterUsers(@Query() queries: FilterUsersDTO, @Res() res: Response) {
         const { name, currentPage, pageSize } = queries;
 
         // filter user
-        const users = await this.userService.filterUsers('%' + name + '%', (currentPage - 1) * pageSize, pageSize);
-        return res.send({ data: users });
+        const users = await this.userService.filterUsers(name, currentPage, pageSize);
+        return res.send(users);
     }
 }
